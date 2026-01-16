461c972949d596edb73361916be0388996509907
rayen189 <73802484+rayen189@users.noreply.github.com>
2026-01-16 14:16:54 -0300

    Update particles.js

--- js/particles.js
+++ js/particles.js
@@ -14,19 +14,19 @@ resizeCanvas();
 
 class Particle {
   constructor() { this.reset(); }
-  reset() {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.radius = Math.random() * 1.8 + 0.6;
-    this.speedY = Math.random() * 0.25 + 0.05;
-    this.alpha = Math.random() * 0.6 + 0.3;
-    this.color = "120, 180, 255";
+  reset(){
+    this.x = Math.random()*canvas.width;
+    this.y = Math.random()*canvas.height;
+    this.radius = Math.random()*1.8+0.6;
+    this.speedY = Math.random()*0.25+0.05;
+    this.alpha = Math.random()*0.6+0.3;
+    this.color="120,180,255";
   }
-  update() {
+  update(){
     this.y -= this.speedY;
-    if(this.y < -10) { this.y = canvas.height + 10; this.x = Math.random()*canvas.width; }
+    if(this.y<-10){ this.y=canvas.height+10; this.x=Math.random()*canvas.width; }
   }
-  draw() {
+  draw(){
     ctx.beginPath();
     ctx.arc(this.x,this.y,this.radius,0,Math.PI*2);
     ctx.fillStyle=`rgba(${this.color},${this.alpha})`;
@@ -34,16 +34,8 @@ class Particle {
   }
 }
 
-function initParticles() {
-  particles = [];
-  for(let i=0;i<PARTICLE_COUNT;i++) particles.push(new Particle());
-}
-
-function animate() {
-  ctx.clearRect(0,0,canvas.width,canvas.height);
-  particles.forEach(p=>{p.update();p.draw();});
-  requestAnimationFrame(animate);
-}
+function initParticles(){ particles=[]; for(let i=0;i<PARTICLE_COUNT;i++){ particles.push(new Particle()); } }
+function animate(){ ctx.clearRect(0,0,canvas.width,canvas.height); particles.forEach(p=>{p.update();p.draw();}); requestAnimationFrame(animate); }
 
 initParticles();
 animate();

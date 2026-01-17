a59fa6d77ec121d53cbddf9e7e3f610ef7c701c0
rayen189 <73802484+rayen189@users.noreply.github.com>
2026-01-16 22:54:53 -0300

    Update particles.js

--- js/particles.js
+++ js/particles.js
@@ -8,28 +8,36 @@ function resize() {
 window.addEventListener("resize", resize);
 resize();
 
-const particles = Array.from({ length: 100 }, () => ({
-  x: Math.random() * canvas.width,
-  y: Math.random() * canvas.height,
-  vx: (Math.random() - 0.5) * 0.3,
-  vy: (Math.random() - 0.5) * 0.3
-}));
+let particles = [];
+
+class Particle {
+  constructor() {
+    this.reset();
+  }
+  reset() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.r = Math.random() * 1.8 + 0.5;
+    this.speed = Math.random() * 0.4 + 0.1;
+    this.alpha = Math.random() * 0.6 + 0.2;
+  }
+  update() {
+    this.y -= this.speed;
+    if (this.y < -10) this.y = canvas.height + 10;
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
+    ctx.fillStyle = `rgba(170,136,255,${this.alpha})`;
+    ctx.fill();
+  }
+}
+
+for (let i = 0; i < 120; i++) particles.push(new Particle());
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  particles.forEach(p => {
-    p.x += p.vx;
-    p.y += p.vy;
-
-    if (p.x < 0 || p.x > canvas.width) p.vx *= -1;
-    if (p.y < 0 || p.y > canvas.height) p.vy *= -1;
-
-    ctx.fillStyle = "#aaffcc";
-    ctx.fillRect(p.x, p.y, 2, 2);
-  });
-
+  particles.forEach(p => { p.update(); p.draw(); });
   requestAnimationFrame(animate);
 }
-
 animate();
